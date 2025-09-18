88760328c38a84e491e18db0dbe19369b6ba6299
Mafunda Frank <frankchimese@gmail.com>
2025-09-18 20:17:33 +0200

    Add files via upload

---
+++ scripts/dropdown.js
@@ -0,0 +1,9 @@
+function on() {
+	document.getElementById("dropdown-menu").style.width = "40%";
+	document.getElementById("dropdown-menu").style.padding = "1rem";
+}
+
+function off() {
+	document.getElementById("dropdown-menu").style.width = "0";
+	document.getElementById("dropdown-menu").style.padding = "0";
+}
