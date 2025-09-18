b6a4e30f1ae63ba497c6ce39700fe68da0f8a93e
Mafunda Frank <frankchimese@gmail.com>
2025-09-18 20:19:38 +0200

    Delete dropdown.js

--- dropdown.js
+++
@@ -1,9 +0,0 @@
-function on() {
-	document.getElementById("dropdown-menu").style.width = "40%";
-	document.getElementById("dropdown-menu").style.padding = "1rem";
-}
-
-function off() {
-	document.getElementById("dropdown-menu").style.width = "0";
-	document.getElementById("dropdown-menu").style.padding = "0";
-}
